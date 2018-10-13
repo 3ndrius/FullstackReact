394c9cf5e5e50ecc8111355cc0188de9b3562f28
3ndrius <andrzejo1991@gmail.com>
2018-10-13 20:55:35 +0200

    Adding project summary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,30 +1,14 @@
 import React from 'react'
 
+import ProjectSummary from './ProjectSummary';
 export default () => {
   return (
     <div classNameName="project-list section mt-5">
 
-    <div className="card text-white bg-primary mb-3">
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-    <div className="card text-white bg-primary mb-3" >
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-    <div className="card text-white bg-primary mb-3">
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+        <ProjectSummary />
+        <ProjectSummary />
+        <ProjectSummary />
+        <ProjectSummary />
       
     </div>
   )
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+  return (
+        <div class="card border-primary mb-3">
+        
+        <div class="card-body">
+            <h4 class="card-title">Primary card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-header">15 July 2018</div>
+        </div>
+  )
+}
