f6dad4e0159d51c5e4943863c9984c0bbba459a8
3ndrius <andrzejo1991@gmail.com>
2018-10-14 16:12:29 +0200

    Adding creteProject component

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class CreateProject extends Component {
+    state = {
+        title: '',
+        content: ''
+    }
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            [e.target.id]:e.target.value
+        })
+    }
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+  render() {
+    return (
+      <div className="container">
+        <form className="p-5" onSubmit={this.handleSubmit} >
+        <h2 className="text-center">Create project </h2>
+            <div className="form-group">
+                <label htmlFor="title">Project name</label>
+                <input type="text" className="form-control" id="title" aria-describedby="titleHelp" placeholder="EnterTitle" onChange={this.handleChange} />
+               
+            </div>
+            <div className="form-group">
+      <label htmlFor="content">Content</label>
+      <textarea className="form-control" id="content" rows="5"></textarea>
+    </div>
+            <button className="btn btn-primary">Create</button>           
+        </form>
+      </div>
+    )
+  }
+}
+export default CreateProject;
